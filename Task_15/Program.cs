c2e7e7bff7079947085fd40531b929a2bb1b6d4a
Egor <egor_onishchuk@mail.ru>
2022-12-23 17:39:41 +0300

    Tasks for 2 lesson solved

---
+++ Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿void DetermineIfDayIsHoliday (int dayOfWeekNumber){
+    if (dayOfWeekNumber < 1 || dayOfWeekNumber > 7){
+        Console.WriteLine($"Цифра {dayOfWeekNumber} не обозначает день недели.");
+    } else{
+        if (dayOfWeekNumber > 5 && dayOfWeekNumber < 8){
+            Console.WriteLine("День недели является выходным.");
+        } else{
+            Console.WriteLine("День не недели является выходным.");
+        }
+    } 
+}
+Console.WriteLine("Введите цифру, обозначающую день недели.");
+int dayOfWeekNumber = int.Parse(Console.ReadLine()!);
+DetermineIfDayIsHoliday(dayOfWeekNumber);
