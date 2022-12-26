b563965bc20ef26421ed0957181fe938f9111bdb
Egor <egor_onishchuk@mail.ru>
2022-12-26 14:55:29 +0300

    Tasks for lesson 3 solved

---
+++ Task_21/Program.cs
@@ -0,0 +1,19 @@
+﻿double GetPointCoordinate (string number, string axis)
+{   
+    Console.WriteLine ($"Введите координату точки {number} на оси {axis}.");
+    return double.Parse(Console.ReadLine()!);
+}
+
+double GetDistanceBetweenPoints (double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    return Math.Sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA) + (zB - zA) * (zB - zA));
+}
+
+double xA = GetPointCoordinate ("A", "x");
+double yA = GetPointCoordinate ("A", "y");
+double zA = GetPointCoordinate ("A", "z");
+double xB = GetPointCoordinate ("B", "x");
+double yB = GetPointCoordinate ("B", "y");
+double zB = GetPointCoordinate ("B", "z");
+double distanceBetweenPoints = GetDistanceBetweenPoints (xA, yA, zA, xB, yB, zB);
+Console.WriteLine ($"Расстояние между двумя точками в пространстве - {distanceBetweenPoints:f}. Если введённые координаты имеют больше двух знаков после запятой, результат округляется до двух знаков после запятой.");
---
+++ Task_23/Program.cs
@@ -0,0 +1,21 @@
+﻿int GetNumberCube (int number)
+{   
+    return number * number * number;
+}
+
+int i = 1;
+Console.WriteLine("Введите число.");
+int N = int.Parse(Console.ReadLine()!);
+while (i <= N)
+{   
+    int numberCube = GetNumberCube (i);
+    if (i == N)
+    {   
+        Console.WriteLine ($"{numberCube}.");
+    }
+    else 
+    {   
+        Console.Write ($"{numberCube}, ");
+    }
+    i++;
+}
