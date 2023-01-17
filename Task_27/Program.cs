7f637a551bce4c8852b1df90d8ab061f99540e44
Egor <egor_onishchuk@mail.ru>
2023-01-17 15:21:34 +0300

    Tasks for lesson 4 solved

---
+++ Task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿int SummarizeNumberDigits (int number)
+{
+    int numberDigitsSummary = 0;
+    while (number != 0)
+    { 
+        numberDigitsSummary += number % 10;
+        number = number / 10;
+    }
+    return numberDigitsSummary;
+}
+
+Console.WriteLine ("Введите число.");
+int number = int.Parse (Console.ReadLine ()!);
+Console.WriteLine ($"Сумма цифр числа {number} - {SummarizeNumberDigits (number)}.");
