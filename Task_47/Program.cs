ce5692171542944baafaf03a0d252e0e25386bc2
Egor <egor_onishchuk@mail.ru>
2023-01-26 18:14:05 +0300

    Tasks for lesson 7 are solved

---
+++ Task_47/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int GetArrayComponent(string component)
+{
+    Console.WriteLine($"Введите {component}.");
+    return int.Parse(Console.ReadLine()!);
+}
+
+double[,] CreateArray(int rows, int columns, int rangeMinNum, int rangeMaxNum, int fractionalPart)
+{
+    double[,] array = new double[rows, columns];
+    Random randomNum = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(randomNum.NextDouble() * (rangeMaxNum - rangeMinNum) + rangeMinNum, fractionalPart);
+        }
+    }
+
+    return array;
+}
+
+void PrintArrayElements(double[,] array)
+{
+    Console.WriteLine("Элементы массива");
+
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (j + 1 == columns)
+            {
+                Console.WriteLine(array[i, j]);
+            }
+            else
+            {
+                Console.Write($"{array[i, j]}, ");
+            }
+        }
+    }
+}
+
+
+int m = GetArrayComponent("количество строк массива");
+int n = GetArrayComponent("количество столбцов массива");
+int rangeMinNum = GetArrayComponent("минимальное число диапазона, в пределах которого будут создаваться псевдослучайные числа для массива");
+int rangeMaxNum = GetArrayComponent("максимальное число диапазона, в пределах которого будут создаваться псевдослучайные числа для массива");
+int fractionalPart = GetArrayComponent("максимальное количество знаков после запятой в числах");
+
+double[,] array = CreateArray(m, n, rangeMinNum, rangeMaxNum, fractionalPart);
+PrintArrayElements(array);
---
+++ Task_50/Program.cs
@@ -0,0 +1,77 @@
+﻿/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет. */
+
+int GetArrayComponent(string component)
+{
+    Console.WriteLine($"Введите {component}.");
+    return int.Parse(Console.ReadLine()!);
+}
+
+double[,] CreateArray(int rows, int columns, int rangeMinNum, int rangeMaxNum, int fractionalPart)
+{
+    double[,] array = new double[rows, columns];
+    Random randomNum = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(randomNum.NextDouble() * (rangeMaxNum - rangeMinNum) + rangeMinNum, fractionalPart);
+        }
+    }
+
+    return array;
+}
+
+void PrintArrayElements(double[,] array)
+{   
+    Console.WriteLine("Элементы массива");
+
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (j + 1 == columns)
+            {
+                Console.WriteLine(array[i, j]);
+            }
+            else
+            {
+                Console.Write($"{array[i, j]}, ");
+            }
+        }
+    }
+}
+
+void FindArrayElementByPosition(double[,] array, int row, int column)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    if (row > rows || column > columns)
+    {
+        Console.WriteLine($"Элемента массива на позиции {row}, {column} не существует.");
+    }
+    else 
+    {
+        Console.WriteLine($"Значение элемента массива на позиции {row}, {column} - {array[row - 1, column - 1]}.");
+    }
+}
+
+
+int rows = GetArrayComponent("количество строк массива");
+int columns = GetArrayComponent("количество столбцов массива");
+int rangeMinNum = GetArrayComponent("минимальное число диапазона, в пределах которого будут создаваться псевдослучайные числа для массива");
+int rangeMaxNum = GetArrayComponent("максимальное число диапазона, в пределах которого будут создаваться псевдослучайные числа для массива");
+int fractionalPart = GetArrayComponent("количество знаков после запятой");
+
+double[,] array = CreateArray(rows, columns, rangeMinNum, rangeMaxNum, fractionalPart);
+PrintArrayElements(array);
+
+int row = GetArrayComponent("строку элемента массива");
+int column = GetArrayComponent("столбец элемента массива");
+
+FindArrayElementByPosition(array, row, column);
