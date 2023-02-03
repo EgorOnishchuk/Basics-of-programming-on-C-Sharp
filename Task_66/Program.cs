8cf05f81d6e50305263b784416ef9b2ff543658d
Egor <egor_onishchuk@mail.ru>
2023-02-03 14:56:21 +0300

    Tasks for lesson 9 are solved

---
+++ Task_66/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetValues(string valueNumber)
+{
+    Console.WriteLine($"Введите {valueNumber} число.");
+    return int.Parse(Console.ReadLine()!);
+}
+
+bool IsNumNatural(double number)
+{
+    if (number <= 0)
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+int GetNaturalNumbersSum(int rangeFirstNum, int rangeLastNum)
+{   
+    if (rangeFirstNum > rangeLastNum)
+    {
+        return 0;
+    }
+
+    return rangeFirstNum + GetNaturalNumbersSum(rangeFirstNum + 1, rangeLastNum);
+}
+
+
+int M = GetValues("первое");
+while(!IsNumNatural(M))
+{
+    M = GetValues("первое");
+}
+
+int N = GetValues("второе");
+while(!IsNumNatural(N))
+{
+    N = GetValues("второе");
+}
+
+Console.WriteLine($"Сумма натуральных чисел от {M} до {N} - {GetNaturalNumbersSum(M, N)}.");
---
+++ Task_68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int GetFunctionArgument(string argumentNumber)
+{
+    Console.WriteLine($"Введите {argumentNumber} неотрицательное число.");
+    return int.Parse(Console.ReadLine()!);
+}
+
+int CalculateAckermannFunction(int firstNumber, int secondNumber)
+{
+    if (firstNumber == 0)
+    {
+        return secondNumber + 1;
+    }
+     
+    if (firstNumber > 0 && secondNumber == 0)
+    {
+        return CalculateAckermannFunction(firstNumber - 1, 1);
+    }
+
+    return CalculateAckermannFunction(firstNumber - 1, CalculateAckermannFunction(firstNumber, secondNumber - 1));
+}
+
+
+int m = GetFunctionArgument("первое");
+int n = GetFunctionArgument("второе");
+
+Console.WriteLine($"Результат вычисления функции Аккермана - {CalculateAckermannFunction(m, n)}.");
