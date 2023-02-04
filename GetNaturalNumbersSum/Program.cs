92a98ab43ce059c77db4db64c6230255bb5ec821
Egor <egor_onishchuk@mail.ru>
2023-02-04 15:28:15 +0300

    2 titles changed

---
+++ GetNaturalNumbersSum/Program.cs
@@ -0,0 +1,30 @@
+﻿int GetValues(string valueNumber)
+{
+    Console.WriteLine($"Введите {valueNumber} число.");
+    int number = int.Parse(Console.ReadLine()!);
+
+    while (number <= 0)
+    {
+        Console.WriteLine($"Введите {valueNumber} число.");
+        number = int.Parse(Console.ReadLine()!);
+    }
+
+    return number;
+}
+
+int GetNaturalNumbersSum(int rangeFirstNum, int rangeLastNum)
+{
+    if (rangeFirstNum > rangeLastNum)
+    {
+        return 0;
+    }
+
+    return rangeFirstNum + GetNaturalNumbersSum(rangeFirstNum + 1, rangeLastNum);
+}
+
+
+int M = GetValues("первое");
+
+int N = GetValues("второе");
+
+Console.WriteLine($"Сумма натуральных чисел от {M} до {N} - {GetNaturalNumbersSum(M, N)}.");
